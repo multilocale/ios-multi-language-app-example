a60fd2f82d613c06e2d3e1fa7bc75b46db04dc4a
Giorgio Zamparelli <giorgio.zamparelli@gmail.com>
2022-12-02 12:47:27 +0100

    feat: create new iOS project with Xcode

---
+++ ios-multi-language-app-example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-multi-language-app-example
+//
+//  Created by Giorgio Zamparelli on 12/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-multi-language-app-example/ios_multi_language_app_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_multi_language_app_exampleApp.swift
+//  ios-multi-language-app-example
+//
+//  Created by Giorgio Zamparelli on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_multi_language_app_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
